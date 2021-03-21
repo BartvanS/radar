b1f9d7a772bdc1172aafdbc975e42efb97cd2d58
BartvanS <bartvansevenbergen@hotmail.com>
2021-03-21 14:42:01 +0100

    badprotocol

--- radar_arduino/src/main.cpp
+++ radar_arduino/src/main.cpp
@@ -4,13 +4,64 @@ void setup()
 {
   Serial.begin(9600);
 }
-char incomingByte = 0;
+const int maxMsgCount = 50;
+char incomingChar = 0;
+char message[maxMsgCount] = {'\0'};
+int msgCount = 0;
+
+enum SerialStates
+{
+  Idle,
+  Receiving,
+  MessageReceived,
+  SendResponse
+};
+SerialStates serialState = Idle;
 void loop()
 {
-  String message 
-  if (Serial.available() > 0)
+  switch (serialState)
   {
-    incomingByte = Serial.read();
-    Serial.print(incomingByte);
+  case Idle:
+  {
+    if (Serial.available() > 0)
+    {
+      incomingChar = Serial.read();
+      if (incomingChar == '#')
+      {
+        serialState = Receiving;
+      }
+    }
+  }
+  case Receiving:
+  {
+    incomingChar = Serial.read();
+    if (incomingChar != '%' || msgCount >= maxMsgCount)
+    {
+      message[msgCount] = incomingChar;
+      msgCount++;
+    }
+    else
+    {
+      msgCount = 0;
+      serialState = MessageReceived;
+    }
+  }
+  break;
+  case MessageReceived:
+  {
+    //parse message and move servo
+
+    serialState = SendResponse;
+  }
+  break;
+  case SendResponse:
+  {
+    //get distance at degree
+    serialState = Idle;
+  }
   }
 }
+
+// {
+
+// Serial.print(incomingByte);
